d5320610897a3029e4f44a9fbee3b4f4df26d02b
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-10-02 21:40:34 +0900

    admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,16 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if user.save
+      flash[:notice]="ユーザー「#{@user.name}」を登録しました。"
+      redirect_to("/admin/users/#{@user.id}")
+    else
+      render("/admin/users/new")
+    end
   end
 
   def edit
@@ -9,5 +20,10 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
