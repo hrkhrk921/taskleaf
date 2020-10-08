566cf10b2a309d8396135e2449d3c8d5a4c5b6d7
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-10-08 16:55:39 +0900

    誤字修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   end
 
   def login_required
-  	redirect_to login_url unless currrent_user
+  	redirect_to login_url unless current_user
   end
 end
