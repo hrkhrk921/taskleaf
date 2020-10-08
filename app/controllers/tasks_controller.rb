fe29917304dceb08f208230341bc8b2c2bd4bdfc
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-10-08 16:48:49 +0900

    共通処理フィルタ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task,only: [:edit,:show,:update,:destroy]
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,7 +12,6 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def create
@@ -26,14 +25,12 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = current_user.tasks.find(params[:id])
     @task.update(task_params)
     flash[:notice]="タスク「#{@task.name}」を更新しました。"
     redirect_to("/")
   end
 
   def destroy
-    @task = current_user.tasks.find(params[:id])
     @task.destroy
     flash[:notice]="タスク「#{@task.name}」を削除しました。"
     redirect_to("/")
@@ -42,5 +39,9 @@ class TasksController < ApplicationController
   private
   def task_params
     params.require(:task).permit(:name,:description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
